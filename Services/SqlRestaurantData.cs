0ae428f42708c32e82b76088252cee9cbcb9fffb
c42-arun <cloud.fortytwo@gmail.com>
2018-09-08 16:04:26 +0100

    DBContext, migrations added

---
+++ Services/SqlRestaurantData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _dbContext;
+
+        public SqlRestaurantData(OdeToFoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _dbContext.Restaurants.Add(restaurant);
+
+            _dbContext.SaveChanges();
+
+            return restaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _dbContext.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _dbContext.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,25 +6,38 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Data;
 using OdeToFood.Services;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            //services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+
+            services.AddDbContext<OdeToFoodDbContext>(
+                options => options.UseSqlServer(_configuration.GetConnectionString("OdeToFood")));
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddSingleton<IGreetService, GreetService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -37,7 +50,7 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                var greeting = configuration["greeting"];
+                var greeting = _configuration["greeting"];
                 await context.Response.WriteAsync(greeting);
             });
         }
