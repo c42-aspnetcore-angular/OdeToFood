73ba1a7749403e979804dc98d2be896310f05b58
c42-arun <cloud.fortytwo@gmail.com>
2018-09-09 15:33:37 +0100

    Page with code-behind as model - including parameters

--- Pages/Greeting.cshtml.cs
+++ Pages/Greeting.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
 
 namespace OdeToFood.Pages
 {
+    // represents the @model for the Razor page for
+    // which this is the code behind file
     public class GreetingModel : PageModel
     {
-        public void OnGet()
+        private IGreetService _greetService;
+
+        public string CurrentGreeting { get; set; }
+
+        public GreetingModel(IGreetService greetService)
         {
+            _greetService = greetService;
+        }
+        public void OnGet(string name)
+        {
+            CurrentGreeting = $"{name} says: {_greetService.GetMessageOfTheDay()} - from Razor Code behind!";
         }
     }
 }
