18191922c05ba48794af42081f1aee87ca1e2db0
c42-arun <cloud.fortytwo@gmail.com>
2018-09-10 15:19:09 +0100

    View component implemented - without tag helper

---
+++ ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreetService _greetService;
+
+        public GreeterViewComponent(IGreetService greetService)
+        {
+            _greetService = greetService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greetService.GetMessageOfTheDay();
+
+            return View("Default", model);
+        }
+    }
+}
